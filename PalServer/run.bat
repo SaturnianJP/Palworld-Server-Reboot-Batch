db6ab9fe3aa0c63bf88c8ed30941bcbcd4bd4caf
さたにあ(黒木 優希) <126487136+SaturnianJP@users.noreply.github.com>
2024-02-14 08:57:16 +0900

    Create run.bat

---
+++ PalServer/run.bat
@@ -0,0 +1,17 @@
+@echo off
+:loop
+echo PalServerを起動します...
+
+REM ファイルが存在するかチェックしています。
+if not exist PalServer.exe (
+    echo Error: PalServer.exe not found.
+    pause
+    exit /b 1
+)
+
+REM ここでプロセス起動、引数を渡す場合はここを編集してください。
+start /wait PalServer.exe -port=8211 -players=32
+echo PalServer has crashed.
+echo Rebooting PalServer.
+goto loop
+
